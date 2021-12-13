7f266959ed28fbdc75e13ecac7db822b2861c7a2
Van-Huy <vanhuy.pham@student.vinci.be>
2021-12-13 15:32:53 +0100

    add - profile page

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,6 +4,7 @@ import LoginPage from "./LoginPage.js";
 import LogoutPage from "./LogoutPage.js";
 import RegisterPage from "./RegisterPage.js";
 import AddRecipePage from "./AddRecipePage.js"
+import ProfilePage from "./ProfilePage.js";
 
 
 const routes = {
@@ -13,6 +14,7 @@ const routes = {
   "/logout": LogoutPage,
   "/register": RegisterPage,
   "/addrecipe": AddRecipePage,
+  "/profile": ProfilePage
 };
 
 let navBar = document.querySelector("#navBar");
